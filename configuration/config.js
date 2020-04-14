7f53249d7ab5411832be0edef65c05835e87536f
Jimelito Rico <jimelitor@hotmail.ph>
2020-04-14 22:01:57 +0800

    added files

---
+++ configuration/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  /*
+   * This file contains the configurations information of Twitter login app.
+   * It consists of Twitter app information, database information.
+   */
+
+  facebook_api_key: "526569948006353",
+  facebook_api_secret: "1f5f04d34b88ade83c20269e6381e59e",
+  callback_url: "https://jimelito-rico.000webhostapp.com/auth/facebook/callback",
+  use_database: false,
+  host: "localhost",
+  username: "root",
+  password: "",
+  database: ""
+};
